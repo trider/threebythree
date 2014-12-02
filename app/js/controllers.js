3dc29a2108c7bafbcad6509279b809936d016548
Jonathang Gold <jonnygold@gmail.com>
2014-12-02 21:03:03 +0200

    Now support for external files and video

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,132 +1,99 @@
 ﻿var tbtAppControllers = angular.module('tbtAppControllers', []);
 
 
-
-tbtAppControllers.controller('tbtCtrl', ['$scope', '$location', 'Submit',
-  function ($scope, $location, Submit)
+tbtAppControllers.controller('tbtCtrl', ['$scope', '$location', 'Submit', 'Questions', 'Media',
+  function ($scope, $location, Submit, Questions, Media)
   {
-    
-
-   $scope.rows =
-    {
-     "row1": [{ "id": "1", "sqid": "#1", "val": "1" }, { "id": "2", "sqid": "#2", "val": "2" }, { "id": "3", "sqid": "#3", "val": "4" }],
-     "row2": [{ "id": "4", "sqid": "#4", "val": "8" }, { "id": "5", "sqid": "#5", "val": "16" }, { "id": "6", "sqid": "#6", "val": "32" }],
-     "row3": [{ "id": "7", "sqid": "#7", "val": "64" }, { "id": "8", "sqid": "#8", "val": "128" }, { "id": "9", "sqid": "#9", "val": "256" }]
-    };
-
-   //$scope.rows =
-   // {
-   //  "row1": [1,2,3],
-   //  "row2": [4,5,6],
-   //  "row3": [7,8,9]
-   // };
-
-   $scope.score = 0;
-   $scope.questions =
-    [
-     {
-      "sqid": "#1", "Question": "1?",
-      "Answers": ["1", "2", "3"],
-      "Answer": "1"
-     },
-     {
-      "sqid": "#2", "Question": "2?",
-      "Answers": ["1", "2", "3"],
-      "Answer": "2"
-     },
-     {
-      "sqid": "#3", "Question": "3?",
-      "Answers": ["1", "2", "3"],
-      "Answer": "3"
-     },
-     {
-      "sqid": "#4", "Question": "4?",
-      "Answers": ["1", "2", "3"],
-      "Answer": "3"
-     },
-     {
-      "sqid": "#5", "Question": "5?",
-      "Answers": ["1", "2", "3"],
-      "Answer": "2"
-     },
-     {
-      "sqid": "#6", "Question": "6?",
-      "Answers": ["1", "2", "3"],
-      "Answer": "1"
-     },
-     {
-      "sqid": "#7", "Question": "7?",
-      "Answers": ["1", "2", "3"],
-      "Answer": "2"
-     },
-     {
-      "sqid": "#8", "Question": "8?",
-      "Answers": ["1", "2", "3"],
-      "Answer": "1"
-     },
-     {
-      "sqid": "#9", "Question": "9?",
-      "Answers": ["1", "2", "3"],
-      "Answer": "3"
-     }
-    
-    ];
-   //$scope.ans = $scope.questions[0].Answer;
-   $scope.scores = new Array();
-   $scope.sqid;
-   $scope.showid = function (id, val) {
-    angular.element('#myModal').modal('toggle');
-    $scope.sqid = '#'+ id;
-    $scope.val = val;
-   };
-
-   $scope.submit = function () {
+   
+    $scope.questions = Questions.query();
+    $scope.round = 1;
+    $scope.score = 0;
+    $scope.vals = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     
     var success = new Audio('data/success.mp3');
     var fail = new Audio('data/fail.mp3');
-    $scope.ans = angular.element('#ans').val();
-    console.log($scope.ans);
-    angular.element('#myModal').modal('toggle');
-
-    $scope.scores;
-
-    $scope.result = Submit.check($scope.sqid, $scope.ans, $scope.questions);
-    if ($scope.result)
-    {
-     var score = 0;
-     $scope.scores.push({ sqid: $scope.sqid, val: $scope.val });
-     //console.log($scope.scores);
-
-     angular.forEach($scope.scores, function (item, key) {
-      score += Number(item.val);
-     });
-     
-     $scope.score = score;
-     
-     angular.element($scope.sqid).text("X");
-     success.play();
-    }
-    else
-    {
-     angular.element($scope.sqid).text("O");
-     fail.play();
-    }
-  
-    if (Submit.isWin($scope.score))
-    {
-     alert("win");
-     var i = 1;
-     angular.forEach($scope.scores, function (item, key) {
-      angular.element(item.sqid).text(i++);
-     });
-     $scope.score = 0;
-     $scope.scores = new Array();
+    $scope.asset = "video/VIDEO0044.mp4";
+    
+    $scope.video = function (e) {
+     var videoElements = angular.element(e.srcElement);
+     videoElements[0].pause();
     }
     
-   }
+    $scope.scores = new Array();
+    $scope.sqid;
 
-  
+    $scope.showid = function (id, val) {
+     $scope.sqid = '#' + id;   
+     if (Submit.IsCellEmpty($scope.sqid))
+     {
+      $scope.val = val;
+      
+      angular.forEach($scope.questions, function (item, key) {
+       if ($scope.sqid == item.sqid) {
+        if (item.display)
+        {
+         console.log(item.url);
+         angular.element('#media').show();
+        }
+        else
+        {
+         angular.element('#media').hide();
+        }
+       }
+      });
+      angular.element('#myModal').modal('toggle');
+     }
 
+    };
+
+    $scope.submit = function () {
+
+     $scope.ans = angular.element('#ans').val();
+     console.log($scope.ans);
+     angular.element('#myModal').modal('toggle');
+
+     $scope.scores;
+
+     $scope.result = Submit.check($scope.sqid, $scope.ans, $scope.questions);
+     
+     if ($scope.result) {
+      var score = 0;
+      $scope.scores.push({ sqid: $scope.sqid, val: $scope.val });
+
+      angular.forEach($scope.scores, function (item, key) {
+       score += Number(item.val);
+      });
+
+      $scope.score = score;
+
+      angular.element($scope.sqid).text("X");
+      success.play();
+     }
+     else {
+      angular.element($scope.sqid).text("O");
+      fail.play();
+     }
+
+     if (Submit.isWin($scope.score)) {
+      alert("סוף סיבוב");
+      var i = 1;
+      angular.forEach($scope.vals, function (item, key) {
+       angular.element('#' + item).text(item);
+      });
+      $scope.score = 0;
+      $scope.scores = new Array();
+      $scope.round++;
+      if ($scope.round > 3) {
+       alert("המשחק הסתיים");
+       $location.path('/tbt');
+       $scope.round = 0;
+      }
+     }
+     
+     
+
+
+    }
 
 
 
--- app/js/services.js
+++ app/js/services.js
@@ -1,6 +1,39 @@
 var tbtAppServices = angular.module('tbtAppServices', ['ngResource']);
 
+tbtAppServices.factory('Questions', ['$resource', function ($resource) {
+
+ return $resource('data/questions.json', {}, {
+  query: { method: 'GET', params: { id: 'question' }, isArray: true }
+ });
+
+}]);
+
+tbtAppServices.service('Media', function () {
+
+ this.showMedia = function (questions, id) {
+  angular.forEach(questions, function (item, key) {
+   if (id == item.sqid) {
+    return item.display;
+   }
+  });
+ }
+
+});
+
+
 tbtAppServices.service('Submit', function () {
+
+ this.IsCellEmpty = function (sqid) {
+  var val = true;
+  var txt = angular.element(sqid).text();
+  if(txt == "X" || txt =="O")
+  {
+   val = false;
+  }
+  return val;
+
+ }
+
 
  this.check = function (id, input, questions) {
   var val = false
@@ -18,9 +51,8 @@ tbtAppServices.service('Submit', function () {
   var vals = [7, 56, 448, 73, 146, 292, 273, 84];
   var win = false;
   angular.forEach(vals, function (item, key) {
-   if(item == val)
-   {
-    win = true
+   if (item == val) {
+    win = true;
    }
   });
   return win;
